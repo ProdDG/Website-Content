23c9b377311471a9ab22a670ce9db96111719e03
HieuLe0301 <s3927205@rmit.edu.vn>
2021-12-19 23:32:02 +0700

    ready for grading

---
+++ script.js
@@ -0,0 +1,8 @@
+var name = document.getElementById("name_field");
+var email = document.getElementById("email_field");
+var num = document.getElementById("num_field");
+var button = document.getElementById("submit");
+
+button.addEventListener("click",function(){
+    alert();
+});
